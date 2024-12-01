8a498824ae92e102389a9acc97501625a95c8273
la-solver <leo.amilie.eth@gmail.com>
2024-12-01 00:21:05 -0500

    Final: App Working

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,6 @@ mongoose
   .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
 // Start the server
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
