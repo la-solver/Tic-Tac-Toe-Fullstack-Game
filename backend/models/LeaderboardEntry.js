a2dc23fccd93280c05e9bbef4b82cdde82c8d9cb
la-solver <leo.amilie.eth@gmail.com>
2024-12-13 16:25:18 -0500

    Final: App Working

--- backend/models/LeaderboardEntry.js
+++ backend/models/LeaderboardEntry.js
@@ -46,8 +46,8 @@ const mongoose = require("mongoose");
  */
 
 const LeaderboardEntrySchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true }, // One entry per user
-  elo: { type: Number, default: 1200 }, // Starting ELO
+  username: { type: String, required: true, unique: true },
+  elo: { type: Number, default: 1200 },
   totalWins: { type: Number, default: 0 },
   totalLosses: { type: Number, default: 0 },
   totalDraws: { type: Number, default: 0 },
